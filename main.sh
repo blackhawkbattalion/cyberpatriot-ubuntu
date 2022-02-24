b7502f7be6c1d7253dbc2a4e1931a144fc81723c
blackhawkbattalion <curranianr@scps.net>
2022-02-24 13:27:38 -0500

    lets hope this works

--- main.sh
+++ main.sh
@@ -4,3 +4,6 @@
 touch /tmp/current_normal_users
 echo "$(getent passwd {1000..6000})" | awk '{print $1}' > /tmp/current_normal_users 
 cat /tmp/current_normal_users
+# This is for a test commit from vscode
+echo "I am commited"
+echo 'lets hope this works!'
